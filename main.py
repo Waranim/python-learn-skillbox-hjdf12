ff871633ce379215539254c452168848b441155d
Waranim <129619485+Waranim@users.noreply.github.com>
2023-11-01 00:54:03 +0500

    edit main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 x = [x for x in range(0, 100)]
-y = np.sin(x) * np.cos(x)
+y = np.log(x) + np.cos(x)
 
 plt.plot(x, y, '-')
 plt.fill_between(x, y, 195, where=(y > 195), facecolor='g', alpha=0.6)
